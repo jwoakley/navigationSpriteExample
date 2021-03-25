06ccd76f1c90276818785c8c8f00c4fc28a89ad6
Joshua Wilder Oakley <43791165+jwoakley@users.noreply.github.com>
2021-03-25 08:07:02 -0700

    updated sketch to include ghost sprite

--- sketch.js
+++ sketch.js
@@ -1,26 +1,23 @@
 /***********************************************************************************
-  Seconds Display
-
-  Uses the p5.timer.js class to show elapsed seconds on the screen
+  simple example of how to implement navigation with a sprite comprised of png files
 
 ------------------------------------------------------------------------------------
-	To use:
-	Add this line to the index.html
+  To use:
+  Add this line to the index.html
 
   <script src="p5.timer.js"></script>
 ***********************************************************************************/
 
 // uninitialized object
-var simpleTimer;
-var elapsedSeconds = 0;
-
 var debugScreen;
 var showDebugScreen = false;
 
+var oddBird;
 var ghost;
 
 function preload() {
-  ghost = loadAnimation('assets/blueblob-01.png', 'assets/blueblob-05.png');
+  oddBird = loadAnimation('assets/blueblob-01.png', 'assets/blueblob-05.png');
+  ghost = loadAnimation('assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
 }
 
 // Setup code goes here
@@ -28,62 +25,64 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   debugScreen = new DebugScreen();
-  simpleTimer = new Timer(1000);
-  simpleTimer.start();
 
-  //animation(ghost, 300, 150);
-
-  // create a sprite and add the 3 animations
-  ghost = createSprite(400, 150, 50, 100);
+  // create a sprite and animations
+  oddBird = createSprite(400, 150);
+  ghost = createSprite(600, 300);
 
   //label, first frame, last frame
   //the addAnimation method returns the added animation
   //that can be store in a temporary variable to change parameters
-  var myAnimation = ghost.addAnimation('floating', 'assets/blueblob-01.png', 'assets/blueblob-05.png');
-
-
-
-  textAlign(CENTER);
-  textSize(96);
+  var myAnimation = oddBird.addAnimation('floating', 'assets/blueblob-01.png', 'assets/blueblob-05.png');
+  var myGhostAnimation = ghost.addAnimation('floating', 'assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
  }
-
 
 // Draw code goes here
 function draw() {
   background(200, 250, 10);
  
-  checkMovement();
+  checkBirdMovement();
+  checkGhostMovement();
 
   //draw the sprite
   drawSprites();
   
-
   if(showDebugScreen ) {
     debugScreen.draw();
   }
 }
 
-function checkMovement() {
+function checkBirdMovement() {
   if(keyIsDown(RIGHT_ARROW))
-    ghost.velocity.x = 1;
+    oddBird.velocity.x = 3;
   else if(keyIsDown(LEFT_ARROW))
-    ghost.velocity.x = -1;
+    oddBird.velocity.x = -3;
+  else
+    oddBird.velocity.x = 0;
+
+  if(keyIsDown(DOWN_ARROW))
+    oddBird.velocity.y = 3;
+  else if(keyIsDown(UP_ARROW))
+    oddBird.velocity.y = -3;
+  else
+    oddBird.velocity.y = 0;
+}
+
+function checkGhostMovement() {
+  if(keyIsDown(RIGHT_ARROW))
+    ghost.velocity.x = 3;
+  else if(keyIsDown(LEFT_ARROW))
+    ghost.velocity.x = -3;
   else
     ghost.velocity.x = 0;
 
   if(keyIsDown(DOWN_ARROW))
-    ghost.velocity.y = 1;
+    ghost.velocity.y = 3;
   else if(keyIsDown(UP_ARROW))
-    ghost.velocity.y = -1;
+    ghost.velocity.y = -3;
   else
     ghost.velocity.y = 0;
-}
-// Looks for elapsed time
-function updateTimer() {
-  if( simpleTimer.expired() ) {
-  	elapsedSeconds++;
-  	simpleTimer.start();
-  }
+
 }
 
 function mouseReleased() {
